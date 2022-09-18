51ca32a0ebcb8ce4bb0e568889f12dbe0187cbc2
Alix Virot <flying.cat@mac.com>
2022-09-18 12:59:49 +0200

    add comments

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // require the Drone model here
-require ("../db/index.js");
-const Drone = require ("../models/Drone.model.js")
+require("../db/index.js");
+const Drone = require("../models/Drone.model.js");
 
 //  ---- Iteration #2: List the drones ----
 // Find the /drones GET route in routes/drones.js. OK // Use the Mongoose .find() method to retrieve all the drones. OK
 // Display all the drones on the drones/list.hbs view. -> with render OK // Make sure you catch the error and output it to the terminal.
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   Drone.find()
-  .then((dbResponse) => {
-    console.log("LOOK Db response :", dbResponse); 
-    res.render("drones/list.hbs", { 
-      drones : dbResponse,
+    .then((dbResponse) => {
+      console.log("LOOK Db response :", dbResponse);
+      res.render("drones/list.hbs", {
+        drones: dbResponse,
+      });
     })
-  })
-  .catch((e)=>console.log(e));
+    .catch((e) => console.log(e));
 });
 
 // Iteration #3.1: Add a new drone
 // render the drones/create-form.hbs view
-router.get('/drones/create', (req, res, next) => {
-  res.render("drones/create-form.hbs") // OK
+router.get("/drones/create", (req, res, next) => {
+  res.render("drones/create-form.hbs"); // OK
 });
 
 // Iteration #3.3: Add a new drone
-// Locate the /drones/create POST route in routes/drones.js // and using - req.body -  get all the info user submitted 
-// through the form. Use this info to create a new drone in // the database in the drones collection. 
-// Make sure you redirect to /drones //  if the new drone is successfully created. 
-// If there is an error, render again the view so the user // can try again to create a drone.
-router.post('/drones/create', (req, res, next) => {
+// /drones/create POST route in routes/drones.js
+// and using - req.body -  =>>> get all the info user submitted
+// through the form. Use this info to create a new drone in
+// the database in the drones collection.
+// Redirect to /drones if the new drone is successfully created.
+// If there is an error, render again the view so the user
+// can try again to create a drone.
+router.post("/drones/create", (req, res, next) => {
   console.log(req.body);
   // const { name, propellers, maxSpeed } = req.body;
   Drone.create(req.body)
@@ -38,50 +41,51 @@ router.post('/drones/create', (req, res, next) => {
       console.log("newDrone: ", newDrone);
       res.redirect("/drones");
     })
-    .catch((e)=>{
+    .catch((e) => {
       console.error(e);
       res.redirect("/drones/create");
-    })
+    });
 });
 
-// Iteration #4.2: --- UPDATE THE DRONE ---
-// Find the /drones/:id/edit GET route in routes/drones.js // and --> render the drones/update-form.hbs view. OK
-// Make sure you get the right drone from the database À VOIR // using the available id (hint: .findById()) 
+// #4.2: --- UPDATE THE DRONE ---
+// Qd on ira sur cette route que l'on définie ici, voilà ce qu'il faut faire :
+// Get the right drone from the db using the available id with .findById()
+// and req.params.id. OK  Render the drones/update-form.hbs view. OK avec info db
 // and pass the drone object to the view.
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   Drone.findById(req.params.id)
-    .then((drone)=> {
-      res.render("drones/update-form.hbs", { drone : drone})
+    .then((drone) => {
+      res.render("drones/update-form.hbs", { drone: drone });
     })
     //.catch(next);
-    .catch((e)=> console.error(e));
+    .catch((e) => console.error(e));
 });
 
- // Iteration #4.4: Update the drone
-//  Using req.body get all the info user submitted //  through the form. Use this info to update the 
-//  existing drone in the database. Make sure you  //  redirect to /drones if the new drone is successfully 
-//  updated. If there is an error, render again the view //  so the user can try again to update a drone. 
-//  (Hint: You can use .findByIdAndUpdate() Mongoose method.)
+// #4.4: Update the drone
+//  Using req.body get all the info user submitted through the form. OK
+// Use this info to update the existing drone in the database.
+// with .findByIdAndUpdate() Mongoose method.
+// Redirect to /drones if the new drone is successfully updated.
+// If there is an error, render again the view so the user can try again.
 // --- j'ai add "async"
-router.post('/drones/:id/edit', async (req, res, next) => {
-  try{
+router.post("/drones/:id/edit", async (req, res, next) => {
+  try {
     await Drone.findByIdAndUpdate(req.params.id, req.body);
     res.redirect("/drones");
-  }catch(err){
+  } catch (err) {
     res.redirect("/drones/create");
   }
 });
 
-// Iteration #5: Delete the drone
-// Find the /drones/:id/delete POST route in routes/drones.js // and using .findByIdAndDelete() (or .findByIdAndRemove()),
-//  destroy the document with the given ID from the database.
-router.post('/drones/:id/delete', (req, res, next) => {
-  const {id} = req.params; // ajout de cette const
+// #5: -- Delete the drone --
+// Use .findByIdAndDelete() or .findByIdAndRemove(), to
+//  destroy the document with the given ID from the db.
+router.post("/drones/:id/delete", (req, res, next) => {
+  const { id } = req.params; // ajout de cette const
   //console.log(req.params.id === id)
   Drone.findByIdAndDelete(id)
-  .then(()=> res.redirect("/drones")) // supprimer (success)
-  .catch((e)=> console.error(e));
+    .then(() => res.redirect("/drones")) // supprimer (success)
+    .catch((e) => console.error(e));
 });
 
 module.exports = router;
-
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,45 +1,44 @@
 // Iteration #1
 
 // ----- REMPLISSAGE DATABASE AVEC SEED -----
-// 1.2  In the seeds/drones.seeds.js file: // Create an array of 3 objects : each with name, 
+// 1.2  In the seeds/drones.seeds.js file: // Create an array of 3 objects : each with name,
 // propellers and maxSpeed as our initial drones. DONE // Establish a connection to the database. DONE
-//You can use the same code in db/index.js. DONE // Once the database connection is established, ... 
-
+//You can use the same code in db/index.js. DONE // Once the database connection is established, ...
 
 // The connection to the database :
-require ("../db/index.js");
-const Drone = require ("../models/Drone.model.js")
-
+require("../db/index.js");
+const Drone = require("../models/Drone.model.js");
 
 const drones = [
-    {
-        name : 'SuperSpider',
-        propellers : 8,
-        maxSpeed : 20,
-    },
-    {
-        name : 'SpicyPhoenix',
-        propellers : 6,
-        maxSpeed : 18,
-    },
-    {
-        name : 'AngryLion',
-        propellers : 4,
-        maxSpeed : 16,
-    },
+  {
+    name: "SuperSpider",
+    propellers: 8,
+    maxSpeed: 20,
+  },
+  {
+    name: "SpicyPhoenix",
+    propellers: 6,
+    maxSpeed: 18,
+  },
+  {
+    name: "AngryLion",
+    propellers: 4,
+    maxSpeed: 16,
+  },
 ];
 
-
-// call the Drone model's .create() method with the array as an argument. // If the .create() method successfully creates the drones collection, 
+// call the Drone model's .create() method with the array as an argument. // If the .create() method successfully creates the drones collection,
 // output (using console.log()) how many drones have been created. // In case, the seeding of the database fails, catch the error and output it.
 
-(async function createDrone(){
-    try { await Drone.deleteMany();
-        const createdDrones = await Drone.create(drones); //DONE
-        console.log(`You've created ${createdDrones.length} BEAUTIFUL drones` ); //D
-        process.exit();
-    } catch (err){ //D
-        console.error(err);
-        process.exit();
-    }
+(async function createDrone() {
+  try {
+    await Drone.deleteMany();
+    const createdDrones = await Drone.create(drones); //DONE
+    console.log(`You've created ${createdDrones.length} BEAUTIFUL drones`); //DONE
+    process.exit();
+  } catch (err) {
+    //DONE
+    console.error(err);
+    process.exit();
+  }
 })();
